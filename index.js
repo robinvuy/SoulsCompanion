3fe27356535476449efd0106c71e55194eb4adf4
Robin Vuyyuru <robinvuy@gmail.com>
2026-04-13 17:25:42 +0100

    made bosses endpoint

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const bosses = [
+    { 
+        id: 1,
+        name: "Sister Freide",
+        game: "Dark Souls 3",
+        difficulty: 5
+    },
+    { 
+        id: 2,
+        name: "Artorias",
+        game: "Dark Souls 1",
+        difficulty: 3
+    },
+    { 
+        id: 3,
+        name: "Orphan of Kos",
+        game: "Bloodborne",
+        difficulty: 5
+    }
+]
+
 app.get('/', (req,res) => {
     res.send("Hello souls player!")
+});
+
+app.get('/bosses', (req,res) => {
+    res.json(bosses)
 });
 
 app.listen(port, () => { 
