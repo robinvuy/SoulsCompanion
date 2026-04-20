ffe40ca5da18e5988a4e2fdfde6ffbb456b7d2b9
Robin Vuyyuru <robinvuy@gmail.com>
2026-04-20 16:15:41 +0100

    [/bosses endpoint with sorting and filters for game and difficulty]

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 8080;
 const bosses = [
     { 
         id: 1,
-        name: "Sister Freide",
+        name: "Sister Friede",
         game: "Dark Souls 3",
         difficulty: 5
     },
@@ -21,6 +21,23 @@ const bosses = [
         name: "Orphan of Kos",
         game: "Bloodborne",
         difficulty: 5
+    },
+    { 
+        id: 4,
+        name: "Crystal Sage",
+        game: "Dark Souls 3",
+        difficulty: 2
+    },
+    { 
+        id: 5,
+        name: "Pinwheel",
+        game: "Dark Souls 1",
+        difficulty: 1
+    },{ 
+        id: 6,
+        name: "Cleric Beast",
+        game: "Bloodborne",
+        difficulty: 2
     }
 ]
 
@@ -29,17 +46,25 @@ app.get('/', (req,res) => {
 });
 
 app.get('/bosses', (req,res) => {
-    const game = (req.query.game)
-    
-    bossData = bosses.filter(boss => boss.game == game)
-
-    if (!game) {return res.json(bosses)};
-
+    const game = (req.query.game);
+    const difficulty = (req.query.difficulty);
+    const sort = (req.query.sort);
+    let bossData = bosses;
 
     if (game) {
-        return res.json(bossData)
+        bossData = bossData.filter(boss => boss.game === game)
+    };
+    
+    if (difficulty) {
+        bossData = bossData.filter(boss => boss.difficulty == difficulty)
+    };
+    
+    if (sort) {
+        bossData = [...bossData].sort((a, b) => (b.difficulty - a.difficulty))
     };
 
+        
+    return res.json(bossData);
 });
 
 app.get('/bosses/:id', (req,res) => {
@@ -47,7 +72,7 @@ app.get('/bosses/:id', (req,res) => {
 
     if  (isNaN(bossId)) {
         return res.send("Invalid Boss Id")
-    }
+    };
 
     bossData = bosses.find(boss => boss.id === bossId)
 
