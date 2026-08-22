fc7617cd4d7b3d32c4e8e8bf6996bb7275280939
Robin V <robinvuy@gmail.com>
2026-08-22 20:08:07 +0100

    connecting backend to frontend

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 const express = require('express');
 const bossesRoutes = require('./routes/bossesRoutes');
+const cors = require("cors")
+
 
 
 const app = express();
 const port = 8080;
+
+app.use(cors())
 
 app.use(express.json());
 
